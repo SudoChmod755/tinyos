ed9a4f6fa82034093b0909e39736f79944452964
SudoChmod755 <740901673@qq.com>
2021-09-09 19:47:56 +0800

    add a console

--- tinyos/kernel/init.c
+++ tinyos/kernel/init.c
@@ -3,11 +3,13 @@
 #include "interrupt.h"
 #include "timer.h"
 #include "memory.h"
+#include "thread.h"
+#include "console.h"
 void init_all(){
     put_str("init_all.\n");
     idt_init();
     mem_init();
     thread_init();
     timer_init();
-    
+    console_init();
 }
